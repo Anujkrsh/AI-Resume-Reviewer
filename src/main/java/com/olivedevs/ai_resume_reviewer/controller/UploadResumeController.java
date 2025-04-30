3a288a5b80bda56a59ea99cb97a641f6aa7e70e0
saloni <kprsaloni0@gmail.com>
2025-04-30 16:41:24 +0530

    Created UploadResumeController with API endpoint

---
+++ src/main/java/com/olivedevs/ai_resume_reviewer/controller/UploadResumeController.java
@@ -0,0 +1,44 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@RestController
+@RequestMapping("/resume-reviewer")
+public class UploadResumeController {
+
+    private static final String UPLOAD_DIR = "uploads";
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadResume(@RequestParam("file") MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        String fileNameInLowerCase = fileName.toLowerCase();
+        if(!fileNameInLowerCase.endsWith(".pdf") && !fileNameInLowerCase.endsWith(".doc") && !fileNameInLowerCase.endsWith(".docx")) {
+            return ResponseEntity.badRequest().body("Invalid file format, only pdf and word documents are supported");
+        }
+        try{
+            Path uploadPath = Paths.get(UPLOAD_DIR);
+            Path filePath = uploadPath.resolve(fileNameInLowerCase);
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+            return ResponseEntity.ok("File uploaded successfully"+fileName);
+        }
+        catch(IOException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("File upload failed."+e.getMessage());
+        }
+    }
+
+
+
+
+}
