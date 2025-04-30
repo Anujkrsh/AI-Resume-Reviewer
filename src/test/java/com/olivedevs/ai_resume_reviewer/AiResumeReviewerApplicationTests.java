647c51a6f7615dff2bb4fff302de67ef76a184a9
Anujkrsh <anujkrsharma@hotmail.com>
2025-04-30 14:57:43 +0530

    initial commit

---
+++ src/test/java/com/olivedevs/ai_resume_reviewer/AiResumeReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivedevs.ai_resume_reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeReviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
